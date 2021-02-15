5dc1a1666812b4d9143261d706770d8ec6733285
tnaoki.09j@gmail.com <ec2-user@ip-172-31-60-7.ec2.internal>
2021-02-15 12:34:11 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
- root to: 'homes#top'
+  get 'books/new'
+  devise_for :users
+  root to: 'homes#top'
+  get 'homes/about'
 end
